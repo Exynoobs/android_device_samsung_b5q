Exynoobs/android_device_samsung_q5q

q5q: import lineage touch HAL

device.mk
@@ -29,6 +29,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     FrameworksResq5q
 
+# Touch features
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch-service.samsung-q5q
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlanmdsp.otaupdate_symlink \

Exynoobs/android_device_samsung_b5q

device.mk
@@ -34,6 +34,10 @@ PRODUCT_PACKAGES += \
     SystemUIResB5q \
     WifiResTargetB5q
 
+# Touch features
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch-service.samsung-b5q
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlanmdsp.otaupdate_symlink \